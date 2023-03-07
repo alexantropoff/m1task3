3bee33638df9cb55d8149627e15a0efaf6fb46f1
Alex Antropoff <alant@idc.md>
2023-03-07 10:41:23 +0200

    Initial Commit

---
+++ m1task3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m1task3
+//
+//  Created by Alex Antropoff on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

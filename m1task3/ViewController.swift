fbd7ca041bdfe55651acca67b805dcce7ec2e638
Alex Antropoff <alantr@gmail.com>
2023-03-08 20:26:13 +0200

    hmm

--- m1task3/ViewController.swift
+++ m1task3/ViewController.swift
@@ -12,33 +12,36 @@ class ViewController: UIViewController {
     let squareView = UIView()
     let slider = UISlider()
     var animator: UIViewPropertyAnimator!
-    var margins: UIEdgeInsets!
     var squareWidth: CGFloat = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.layoutMargins=UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16 )
-        margins=view.layoutMargins
-        squareWidth = view.frame.width/6
-        squareView.frame = CGRect(x: margins.left, y: margins.top + squareWidth, width: squareWidth, height: squareWidth)
-        squareView.layer.cornerRadius = 8
-        squareView.backgroundColor = .blue
-        
-        slider.frame = CGRect(x: margins.left, y: margins.top + squareWidth*2 + 30, width: view.frame.width - margins.left - margins.right, height: 20)
-        
         view.addSubview(squareView)
         view.addSubview(slider)
         
         animator = UIViewPropertyAnimator(duration: 1, curve: .easeInOut)
         animator.pausesOnCompletion = true
-        animator.addAnimations {
-            self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2).scaledBy(x: 1.5, y: 1.5)
-            self.squareView.frame.origin.x = self.view.frame.width-self.squareView.frame.width-self.margins.right
-        }
         slider.addTarget(self, action: #selector(releaseSlider), for: .touchUpInside)
         slider.addTarget(self, action: #selector(releaseSlider), for: .touchDragOutside)
         slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
     }
-    
+        
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if(slider.isTracking){return}
+        slider.value=slider.minimumValue
+        squareWidth = view.frame.width/6
+        squareView.frame = CGRect(x: view.layoutMargins.left, y: view.layoutMargins.top + squareWidth, width: squareWidth, height: squareWidth)
+        squareView.layer.cornerRadius = 8
+        squareView.backgroundColor = .blue
+        
+        slider.frame = CGRect(x: view.layoutMargins.left, y: view.layoutMargins.top + squareWidth*2 + 30, width: view.frame.width - view.layoutMargins.left - view.layoutMargins.right, height: 20)
+        animator.stopAnimation(true)
+        squareView.transform = .identity
+        animator.addAnimations {
+            self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2).scaledBy(x: 1.5, y: 1.5)
+            self.squareView.frame.origin.x = self.view.frame.width-self.squareView.frame.width-self.view.layoutMargins.right
+        }
+    }
     @objc func sliderValueChanged() {
         animator.fractionComplete = CGFloat( slider.value )
     }
